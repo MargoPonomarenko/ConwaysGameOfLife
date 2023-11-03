d61b83fbc4bc5f32e34dc3c03687e6bebb8471dc
Marharyta Ponomarenko <margo.marharet@gmail.com>
2023-11-03 11:10:22 +0200

    fix: added absolute path definitions for tests #2

--- src/KPI_Lab1/KPI_Lab1_Test/test.cpp
+++ src/KPI_Lab1/KPI_Lab1_Test/test.cpp
@@ -1,8 +1,19 @@
 #include "pch.h"
 #include "../KPI_Lab1/KPI_Lab1.h"
 
+TEST(AlgorithmTest, CheckFileExistence) {
+	/*std::string dataFileAbsolutePath = "${{ github.workspace }}/src/KPI_Lab1/KPI_Lab1/data.txt";*/
+	EXPECT_EQ(isFileExist("${{ github.workspace }}\src\KPI_Lab1\KPI_Lab1\data.txt"), true);
+	/*EXPECT_EQ(isFileExist("../../KPI_Lab1/data.txt"), true);*/
+	/*EXPECT_EQ(isFileExist(dataFileAbsolutePath), true);*/
+}
+
+TEST(AlgorithmTest, CheckFileNonExistence) {
+	EXPECT_EQ(isFileExist(""), false);
+	EXPECT_EQ(isFileExist("data.txt"), false);
+}
 TEST(AlgorithmTest, CreateVectorFromFileData) {
-	std::string dataFileAbsolutePath = "${{ github.workspace }}/src/KPI_Lab1/KPI_Lab1/data.txt";
+	std::string dataFileAbsolutePath = "${{ github.workspace }}\src\KPI_Lab1\KPI_Lab1\data.txt";
 	vector<int> initData = dataInitialization(dataFileAbsolutePath);
 	/*vector<int> initData = dataInitialization("../../KPI_Lab1/data.txt");*/
 	int actualLength = initData.size();
@@ -14,12 +25,4 @@ TEST(AlgorithmTest, CreateVectorFromFileData) {
 	EXPECT_EQ(initData, expectedData);
 }
 
-TEST(AlgorithmTest, CheckFileExistence) {
-	std::string dataFileAbsolutePath = "${{ github.workspace }}/src/KPI_Lab1/KPI_Lab1/data.txt";
-	/*EXPECT_EQ(isFileExist("../../KPI_Lab1/data.txt"), true);*/
-	EXPECT_EQ(isFileExist(dataFileAbsolutePath), true);
-}
-TEST(AlgorithmTest, CheckFileNonExistence) {
-	EXPECT_EQ(isFileExist(""), false);
-	EXPECT_EQ(isFileExist("data.txt"), false);
-}
+
